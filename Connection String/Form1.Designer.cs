1d591a2f00d9ebb6cd48b68d064b2139adb151f3
Danish Rehan <Danish.Rehan@tpsonline.com>
2019-08-22 11:54:13 +0500

    Application with basic functionality

---
+++ Connection String/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Connection_String
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.browse = new System.Windows.Forms.Button();
+            this.pathText = new System.Windows.Forms.TextBox();
+            this.rep = new System.Windows.Forms.Button();
+            this.repText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.reg = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // browse
+            // 
+            this.browse.Location = new System.Drawing.Point(460, 255);
+            this.browse.Name = "browse";
+            this.browse.Size = new System.Drawing.Size(75, 23);
+            this.browse.TabIndex = 0;
+            this.browse.Text = "Browse";
+            this.browse.UseVisualStyleBackColor = true;
+            this.browse.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // pathText
+            // 
+            this.pathText.Location = new System.Drawing.Point(169, 257);
+            this.pathText.Name = "pathText";
+            this.pathText.Size = new System.Drawing.Size(262, 20);
+            this.pathText.TabIndex = 1;
+            // 
+            // rep
+            // 
+            this.rep.Location = new System.Drawing.Point(460, 306);
+            this.rep.Name = "rep";
+            this.rep.Size = new System.Drawing.Size(75, 23);
+            this.rep.TabIndex = 2;
+            this.rep.Text = "Replace All";
+            this.rep.UseVisualStyleBackColor = true;
+            this.rep.Click += new System.EventHandler(this.Rep_Click);
+            // 
+            // repText
+            // 
+            this.repText.Location = new System.Drawing.Point(169, 306);
+            this.repText.Name = "repText";
+            this.repText.Size = new System.Drawing.Size(262, 20);
+            this.repText.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(284, 368);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(46, 257);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Directory:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(46, 307);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Replace With:";
+            // 
+            // reg
+            // 
+            this.reg.AutoSize = true;
+            this.reg.Location = new System.Drawing.Point(559, 311);
+            this.reg.Name = "reg";
+            this.reg.Size = new System.Drawing.Size(107, 17);
+            this.reg.TabIndex = 7;
+            this.reg.Text = "Replace Registry";
+            this.reg.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.repText);
+            this.Controls.Add(this.rep);
+            this.Controls.Add(this.pathText);
+            this.Controls.Add(this.browse);
+            this.Name = "Form1";
+            this.Text = "XML String Changer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button browse;
+        private System.Windows.Forms.TextBox pathText;
+        private System.Windows.Forms.Button rep;
+        private System.Windows.Forms.TextBox repText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox reg;
+    }
+}
+
---
+++ Connection String/Form1.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Connection_String
+{
+    public partial class Form1 : Form
+    {
+        string directory;
+        string[] repEntries;
+         
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //foreach (FileInfo foundFile in filesInDir)
+            //{
+            //    string fullName = foundFile.FullName;
+            //    Console.WriteLine(fullName);
+            //}
+
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = "D:\\Environments\\Application\\ASKARI\\MonetaBackOffice\\Applications";
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                //MessageBox.Show("You selected: " + dialog.FileName);
+                pathText.Text = directory = dialog.FileName;
+            }
+
+            string[] entries = Directory.GetFileSystemEntries(directory, "Web.config", SearchOption.AllDirectories);
+            repEntries = entries;
+
+            for (int i = 0; i < entries.Length; i++)
+            {
+                string[] spl = entries[i].Split('\\');
+                //Create label
+                Label label = new Label();
+                label.Text = String.Format(spl[spl.Length - 2] + " - Web.config found.");
+                label.AutoSize = true;
+                Console.WriteLine(spl[spl.Length - 2]);
+                //Position label on screen
+                label.Left = 20;
+                label.Top = (i + 1) * 22;
+                Button btn = new Button();
+                btn.Text = String.Format("Replace");
+                btn.Left = 240;
+                btn.Top = (i + 1) * 22;
+                //Create textbox
+                //TextBox textBox = new TextBox();
+                //Position textbox on screen
+                //textBox.Left = 120;
+                //textBox.Top = (i + 1) * 20;
+                ///Add controls to form
+                this.Controls.Add(label);
+                this.Controls.Add(btn);
+                //this.Controls.Add(textBox);
+            }
+        }
+
+        private void Rep_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < repEntries.Length; i++)
+            {
+                string appPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                string configFile = System.IO.Path.Combine(repEntries[i], "");
+                ExeConfigurationFileMap configFileMap = new ExeConfigurationFileMap();
+                configFileMap.ExeConfigFilename = configFile;
+                System.Configuration.Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configFileMap, ConfigurationUserLevel.None);
+                bool key = config.AppSettings.Settings.AllKeys.Contains("ConnString");
+                if (key)
+                {
+                    config.AppSettings.Settings["ConnString"].Value = repText.Text;
+                    config.Save();
+                }
+            }
+
+            if (reg.Checked)
+            {
+
+                using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+                using (var myKey1 = hklm.OpenSubKey(@"SOFTWARE\TPS\Iris", true))
+                {
+                    if (myKey1 != null)
+                    {
+                        Console.WriteLine("Inside!!!");
+                        myKey1.SetValue("ConnectionString", repText.Text, RegistryValueKind.String);
+                        myKey1.Close();
+                    }
+                }
+                using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
+                using (var myKey2 = hklm.OpenSubKey(@"SOFTWARE\TPS\Moneta", true))
+                {
+                    if (myKey2 != null)
+                    {
+                        Console.WriteLine("Inside!!!");
+                        myKey2.SetValue("ConnectionString", repText.Text, RegistryValueKind.String);
+                        myKey2.Close();
+                    }
+                }
+            }
+            label1.Visible = true;
+            label1.BackColor = System.Drawing.Color.Red;
+            label1.Text = "Done !!!";
+        }
+    }
+}
+
