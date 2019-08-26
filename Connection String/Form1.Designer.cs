9ad56e2602f1600fe1e2c1e5b5bab4880f77f680
Danish Rehan <Danish.Rehan@tpsonline.com>
2019-08-26 16:27:38 +0500

    Further functionality added & minor bug fixes.

--- Connection String/Form1.Designer.cs
+++ Connection String/Form1.Designer.cs
@@ -32,17 +32,20 @@
             this.pathText = new System.Windows.Forms.TextBox();
             this.rep = new System.Windows.Forms.Button();
             this.repText = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            this.msgText = new System.Windows.Forms.Label();
             this.dirLabel = new System.Windows.Forms.Label();
             this.repLabel = new System.Windows.Forms.Label();
             this.regCheckBox = new System.Windows.Forms.CheckBox();
             this.label2 = new System.Windows.Forms.Label();
             this.xmlCombo = new System.Windows.Forms.ComboBox();
+            this.exText = new System.Windows.Forms.Label();
+            this.radioWeb = new System.Windows.Forms.RadioButton();
+            this.radioWin = new System.Windows.Forms.RadioButton();
             this.SuspendLayout();
             // 
             // browse
             // 
-            this.browse.Location = new System.Drawing.Point(562, 255);
+            this.browse.Location = new System.Drawing.Point(638, 255);
             this.browse.Name = "browse";
             this.browse.Size = new System.Drawing.Size(75, 23);
             this.browse.TabIndex = 0;
@@ -52,14 +55,15 @@
             // 
             // pathText
             // 
-            this.pathText.Location = new System.Drawing.Point(181, 257);
+            this.pathText.Location = new System.Drawing.Point(257, 257);
             this.pathText.Name = "pathText";
+            this.pathText.ReadOnly = true;
             this.pathText.Size = new System.Drawing.Size(375, 20);
             this.pathText.TabIndex = 1;
             // 
             // rep
             // 
-            this.rep.Location = new System.Drawing.Point(562, 321);
+            this.rep.Location = new System.Drawing.Point(638, 321);
             this.rep.Name = "rep";
             this.rep.Size = new System.Drawing.Size(75, 23);
             this.rep.TabIndex = 2;
@@ -69,26 +73,28 @@
             // 
             // repText
             // 
-            this.repText.Location = new System.Drawing.Point(181, 322);
+            this.repText.Location = new System.Drawing.Point(257, 322);
             this.repText.Name = "repText";
             this.repText.Size = new System.Drawing.Size(375, 20);
             this.repText.TabIndex = 3;
             // 
-            // label1
+            // msgText
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(357, 393);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "label1";
-            this.label1.Visible = false;
+            this.msgText.AutoSize = true;
+            this.msgText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.msgText.ForeColor = System.Drawing.Color.Green;
+            this.msgText.Location = new System.Drawing.Point(322, 393);
+            this.msgText.Name = "msgText";
+            this.msgText.Size = new System.Drawing.Size(191, 16);
+            this.msgText.TabIndex = 4;
+            this.msgText.Text = "Tags have been replaced!";
+            this.msgText.Visible = false;
             // 
             // dirLabel
             // 
             this.dirLabel.AutoSize = true;
             this.dirLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dirLabel.Location = new System.Drawing.Point(46, 261);
+            this.dirLabel.Location = new System.Drawing.Point(122, 261);
             this.dirLabel.Name = "dirLabel";
             this.dirLabel.Size = new System.Drawing.Size(75, 16);
             this.dirLabel.TabIndex = 5;
@@ -98,7 +104,7 @@
             // 
             this.repLabel.AutoSize = true;
             this.repLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.repLabel.Location = new System.Drawing.Point(46, 326);
+            this.repLabel.Location = new System.Drawing.Point(122, 326);
             this.repLabel.Name = "repLabel";
             this.repLabel.Size = new System.Drawing.Size(115, 16);
             this.repLabel.TabIndex = 6;
@@ -107,18 +113,19 @@
             // regCheckBox
             // 
             this.regCheckBox.AutoSize = true;
-            this.regCheckBox.Location = new System.Drawing.Point(326, 292);
+            this.regCheckBox.Location = new System.Drawing.Point(402, 292);
             this.regCheckBox.Name = "regCheckBox";
             this.regCheckBox.Size = new System.Drawing.Size(107, 17);
             this.regCheckBox.TabIndex = 7;
             this.regCheckBox.Text = "Replace Registry";
             this.regCheckBox.UseVisualStyleBackColor = true;
+            this.regCheckBox.CheckedChanged += new System.EventHandler(this.RegCheckBox_CheckedChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(46, 294);
+            this.label2.Location = new System.Drawing.Point(122, 294);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(71, 16);
             this.label2.TabIndex = 8;
@@ -148,23 +155,62 @@
             "defaultFailDir",
             "defaultRTGSDir",
             "defaultRTGSDirAfterApend"});
-            this.xmlCombo.Location = new System.Drawing.Point(181, 289);
+            this.xmlCombo.Location = new System.Drawing.Point(257, 289);
             this.xmlCombo.Name = "xmlCombo";
             this.xmlCombo.Size = new System.Drawing.Size(138, 21);
             this.xmlCombo.TabIndex = 9;
             this.xmlCombo.SelectedIndexChanged += new System.EventHandler(this.XmlCombo_SelectedIndexChanged);
             // 
+            // exText
+            // 
+            this.exText.AutoSize = true;
+            this.exText.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.exText.ForeColor = System.Drawing.Color.Red;
+            this.exText.Location = new System.Drawing.Point(12, 358);
+            this.exText.Name = "exText";
+            this.exText.Size = new System.Drawing.Size(54, 13);
+            this.exText.TabIndex = 10;
+            this.exText.Text = "Exception";
+            this.exText.Visible = false;
+            // 
+            // radioWeb
+            // 
+            this.radioWeb.AutoSize = true;
+            this.radioWeb.Location = new System.Drawing.Point(302, 225);
+            this.radioWeb.Name = "radioWeb";
+            this.radioWeb.Size = new System.Drawing.Size(92, 17);
+            this.radioWeb.TabIndex = 11;
+            this.radioWeb.TabStop = true;
+            this.radioWeb.Text = "Web Services";
+            this.radioWeb.UseVisualStyleBackColor = true;
+            this.radioWeb.CheckedChanged += new System.EventHandler(this.RadioWeb_CheckedChanged);
+            // 
+            // radioWin
+            // 
+            this.radioWin.AutoSize = true;
+            this.radioWin.Location = new System.Drawing.Point(419, 225);
+            this.radioWin.Name = "radioWin";
+            this.radioWin.Size = new System.Drawing.Size(113, 17);
+            this.radioWin.TabIndex = 12;
+            this.radioWin.TabStop = true;
+            this.radioWin.Text = "Windows Services";
+            this.radioWin.UseVisualStyleBackColor = true;
+            this.radioWin.CheckedChanged += new System.EventHandler(this.RadioWin_CheckedChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(835, 467);
+            this.Controls.Add(this.radioWin);
+            this.Controls.Add(this.radioWeb);
+            this.Controls.Add(this.exText);
             this.Controls.Add(this.xmlCombo);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.regCheckBox);
             this.Controls.Add(this.repLabel);
             this.Controls.Add(this.dirLabel);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.msgText);
             this.Controls.Add(this.repText);
             this.Controls.Add(this.rep);
             this.Controls.Add(this.pathText);
@@ -182,12 +228,15 @@
         private System.Windows.Forms.TextBox pathText;
         private System.Windows.Forms.Button rep;
         private System.Windows.Forms.TextBox repText;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label msgText;
         private System.Windows.Forms.Label dirLabel;
         private System.Windows.Forms.Label repLabel;
         private System.Windows.Forms.CheckBox regCheckBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox xmlCombo;
+        private System.Windows.Forms.Label exText;
+        private System.Windows.Forms.RadioButton radioWeb;
+        private System.Windows.Forms.RadioButton radioWin;
     }
 }
 
--- Connection String/Form1.cs
+++ Connection String/Form1.cs
@@ -12,29 +12,33 @@ namespace Connection_String
     public partial class Form1 : Form
     {
         string directory;
-        string[] repEntries;
-         
+        string[] entries;
+        IEnumerable<string> files;
+
         public Form1()
         {
             InitializeComponent();
+            regCheckBox.Enabled = false;
+            rep.Enabled = false;
+            radioWeb.Checked = true;
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            //foreach (FileInfo foundFile in filesInDir)
-            //{
-            //    string fullName = foundFile.FullName;
-            //    Console.WriteLine(fullName);
-            //}
-            //MessageBox.Show(this.Controls.GetType().Name);
-            int j = 0;
-            while (j < this.Controls.Count)
+            entries = null;
+            files = null;
+            msgText.Visible = false;
+            exText.Visible = false;
+
+            for(int i = 0; i < this.Controls.Count; i++)
             {
-                var labelToRemove = this.Controls["lbl1" + j];
-                var buttonToRemove = this.Controls["btn1" + j];
-                this.Controls.Remove(labelToRemove);
-                this.Controls.Remove(buttonToRemove);
-                j++;
+                if (this.Controls.ContainsKey("lbl1" + i))
+                {
+                    System.Windows.Forms.Control[] ad = new System.Windows.Forms.Control[1];
+                    ad = Controls.Find("lbl1" + i, true);
+                    ad[0].Dispose();
+                    var a = Controls.Find("lbl1" + i, true);
+                }
             }
             CommonOpenFileDialog dialog = new CommonOpenFileDialog();
             dialog.InitialDirectory = "D:\\Environments\\Application\\ASKARI\\MonetaBackOffice\\Applications";
@@ -43,66 +47,136 @@ namespace Connection_String
             {
                 //MessageBox.Show("You selected: " + dialog.FileName);
                 pathText.Text = directory = dialog.FileName;
+                rep.Enabled = true;
             }
 
-            string[] entries = Directory.GetFileSystemEntries(directory, "Web.config", SearchOption.AllDirectories);
-            repEntries = entries;
-
-            for (int i = 0; i < entries.Length; i++)
+            if (radioWeb.Checked)
             {
-                string[] spl = entries[i].Split('\\');
-                //Create label
-                Label label = new Label();
-                label.Text = String.Format(spl[spl.Length - 2] + " - Web.config found.");
-                label.Name = "lbl1" + i;
-                label.AutoSize = true;
-                //Console.WriteLine(spl[spl.Length - 2]);
-                //Position label on screen
-                label.Left = 20;
-                label.Top = (i + 1) * 22;
-                Button btn = new Button();
-                btn.Text = String.Format("Replace");
-                btn.Name = "btn1" + i;
-                btn.Left = 240;
-                btn.Top = (i + 1) * 22;
-                //Create textbox
-                //TextBox textBox = new TextBox();
-                //Position textbox on screen
-                //textBox.Left = 120;
-                //textBox.Top = (i + 1) * 20;
-                ///Add controls to form
-                this.Controls.Add(label);
-                this.Controls.Add(btn);
-                //this.Controls.Add(textBox);
+                try
+                {
+                    entries = Directory.GetFileSystemEntries(directory, "Web.config", SearchOption.AllDirectories);
+                    //Console.WriteLine(String.Format(entries[0]));
+                }
+                catch (Exception ex)
+                {
+                   
+                }
+            }
+
+            if(radioWin.Checked)
+            {
+                files = Directory.EnumerateFiles(directory, "*.config", SearchOption.AllDirectories)
+                .Where(s => s.EndsWith("AMLService.exe.config") || s.EndsWith("AutoCleanseService.exe.config") ||
+                s.EndsWith("DailyExportService.exe.config") || s.EndsWith("FundsTransferService.exe.config") ||
+                s.EndsWith("ImportService.exe.config") || s.EndsWith("VMSBOContinousMailing.exe.config") ||
+                s.EndsWith("ProcessingService.exe.config") || s.EndsWith("SmsService.exe.config") ||
+                s.EndsWith("ThirdPartyExportService.exe.config")
+                );
+                /*foreach (string a in files)
+                {
+                    Console.WriteLine(a);
+                }*/
+            }
+
+            if (entries != null)
+            {
+                for (int i = 0; i < entries.Length; i++)
+                {
+                    string[] spl = entries[i].Split('\\');
+                    //Create label
+                    Label label = new Label();
+                    label.Text = String.Format(spl[spl.Length - 2] + " - Web.config found.");
+                    label.Name = "lbl1" + i;
+                    Console.WriteLine(label.Name);
+                    label.AutoSize = true;
+                    //Console.WriteLine(spl[spl.Length - 2]);
+                    //Position label on screen
+                    label.Left = 20;
+                    label.Top = (i + 1) * 22;
+                    Button btn = new Button();
+                    btn.Text = String.Format("Replace");
+                    btn.Name = "btn1" + i;
+                    btn.Left = 240;
+                    btn.Top = (i + 1) * 22;
+                    //Create textbox
+                    //TextBox textBox = new TextBox();
+                    //Position textbox on screen
+                    //textBox.Left = 120;
+                    //textBox.Top = (i + 1) * 20;
+                    ///Add controls to form
+                    this.Controls.Add(label);
+                    //this.Controls.Add(btn);
+                    //this.Controls.Add(textBox);
+                }
+            }
+
+            if (files != null)
+            {
+                int i = 0;
+                foreach (string a in files)
+                {
+                    string[] spl = a.Split('\\');
+                    //Create label
+                    //Console.WriteLine(a);
+                    Label label = new Label();
+                    label.Text = String.Format(spl[spl.Length - 2] + " - exe.config found.");
+                    label.Name = "lbl1" + i;
+                    label.AutoSize = true;
+                    //Console.WriteLine(spl[spl.Length - 2]);
+                    //Position label on screen
+                    label.Left = 20;
+                    label.Top = (i + 1) * 22;
+                    ///Add controls to form
+                    this.Controls.Add(label);
+                    i++;
+                }
             }
             //MessageBox.Show(this.Controls.Count.ToString());
         }
 
         private void Rep_Click(object sender, EventArgs e)
         {
-            for (int i = 0; i < repEntries.Length; i++)
+            if (radioWeb.Checked)
             {
-                string appPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-                string configFile = System.IO.Path.Combine(repEntries[i], "");
-                ExeConfigurationFileMap configFileMap = new ExeConfigurationFileMap();
-                configFileMap.ExeConfigFilename = configFile;
-                System.Configuration.Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configFileMap, ConfigurationUserLevel.None);
-                bool key = config.AppSettings.Settings.AllKeys.Contains(xmlCombo.Text);
-                if (key)
+                for (int i = 0; i < entries.Length; i++)
                 {
-                    config.AppSettings.Settings[xmlCombo.Text].Value = repText.Text;
-                    config.Save();
+                    string appPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                    string configFile = System.IO.Path.Combine(entries[i], "");
+                    ExeConfigurationFileMap configFileMap = new ExeConfigurationFileMap();
+                    configFileMap.ExeConfigFilename = configFile;
+                    System.Configuration.Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configFileMap, ConfigurationUserLevel.None);
+                    bool key = config.AppSettings.Settings.AllKeys.Contains(xmlCombo.Text);
+                    if (key)
+                    {
+                        config.AppSettings.Settings[xmlCombo.Text].Value = repText.Text;
+                        config.Save();
+                    }
                 }
             }
-
-            if (regCheckBox.Checked)
+            else
+            {
+                foreach (string file in files)
+                {
+                    string appPath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+                    string configFile = System.IO.Path.Combine(file, "");
+                    ExeConfigurationFileMap configFileMap = new ExeConfigurationFileMap();
+                    configFileMap.ExeConfigFilename = configFile;
+                    System.Configuration.Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configFileMap, ConfigurationUserLevel.None);
+                    bool key = config.AppSettings.Settings.AllKeys.Contains(xmlCombo.Text);
+                    if (key)
+                    {
+                        config.AppSettings.Settings[xmlCombo.Text].Value = repText.Text;
+                        config.Save();
+                    }
+                }
+            }
+            if (regCheckBox.Checked && regCheckBox.Enabled)
             {
                 using (var hklm = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64))
                 using (var myKey1 = hklm.OpenSubKey(@"SOFTWARE\TPS\Iris", true))
                 {
                     if (myKey1 != null)
                     {
-                        Console.WriteLine("Inside!!!");
                         myKey1.SetValue("ConnectionString", repText.Text, RegistryValueKind.String);
                         myKey1.Close();
                     }
@@ -112,20 +186,19 @@ namespace Connection_String
                 {
                     if (myKey2 != null)
                     {
-                        Console.WriteLine("Inside!!!");
                         myKey2.SetValue("ConnectionString", repText.Text, RegistryValueKind.String);
                         myKey2.Close();
                     }
                 }
             }
-            label1.Text = "Done !!!";
-            label1.Visible = true;
-            label1.BackColor = System.Drawing.Color.Green;
+            //MessageBox.Show("Tags have been replaced.");
+            msgText.Text = "Tags have been replaced!";
+            msgText.Visible = true;
         }
 
         private void XmlCombo_SelectedIndexChanged(object sender, EventArgs e)
         {
-            if(!(xmlCombo.SelectedItem.ToString() == "ConnString"))
+            if (!(xmlCombo.SelectedItem.ToString() == "ConnString"))
             {
                 regCheckBox.Checked = false;
                 regCheckBox.Enabled = false;
@@ -135,6 +208,34 @@ namespace Connection_String
                 regCheckBox.Enabled = true;
             }
         }
+
+        private void RegCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (xmlCombo.SelectedItem == null)
+            {
+                regCheckBox.Checked = false;
+                regCheckBox.Enabled = false;
+            }
+        }
+
+        private void RadioWeb_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioWeb.Checked)
+            {
+                radioWin.Checked = false;
+                directory = pathText.Text = "";
+                rep.Enabled = false;
+            }
+        }
+
+        private void RadioWin_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioWin.Checked)
+            {
+                radioWeb.Checked = false;
+                directory = pathText.Text = "";
+                rep.Enabled = false;
+            }
+        }
     }
 }
-
